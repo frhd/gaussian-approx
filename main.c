1eacd574e2623397a43643bb71011525cc291440
Farhad Omid <farhadino@gmail.com>
2013-06-22 16:37:12 +0200

    test identity matrix

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include "matrix.h"
+
 int main(int argc, char *argv[]) {
+	Matrix m;
+
+	printf("matrix test\n");
+	m = unitMatrix(3, 3);
+	printMatrix(m);
+	freeMatrix(m);
+
 	return 0;
 }
