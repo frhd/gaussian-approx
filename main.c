fdd32229fee17523a0634652e87f6aa6f1bf927a
Farhad Omid <farhadino@gmail.com>
2013-06-22 16:48:03 +0200

    test zero and identity matrices

--- main.c
+++ main.c
@@ -4,8 +4,15 @@
 int main(int argc, char *argv[]) {
 	Matrix m;
 
-	printf("matrix test\n");
+	printf("=== matrix tests ===\n\n");
+
+	printf("identity 3x3:\n");
 	m = unitMatrix(3, 3);
+	printMatrix(m);
+	freeMatrix(m);
+
+	printf("\nzero 2x4:\n");
+	m = zeroMatrix(2, 4);
 	printMatrix(m);
 	freeMatrix(m);
 
