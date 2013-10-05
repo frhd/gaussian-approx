032704c2b3255cdb1afd8331add6287061a1cee3
Farhad Omid <farhadino@gmail.com>
2013-10-05 20:48:22 +0200

    viz_bar basic impl

--- viz.c
+++ viz.c
@@ -2,5 +2,14 @@
 #include "viz.h"
 
 void viz_bar(float value, float max_val, int width) {
-	printf("TODO\n");
+	int i, n, center;
+	if (max_val <= 0) max_val = 1.0;
+
+	center = width / 2;
+	n = (int)(value / max_val * (width / 2));
+	if (n > width / 2) n = width / 2;
+	if (n < 0) n = 0;
+	for (i = 0; i < center; i++) printf(" ");
+	for (i = 0; i < n; i++) printf("#");
+	printf("\n");
 }
