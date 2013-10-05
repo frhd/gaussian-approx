6659b313e8d754719facf09ca83a9a06b0b66633
Farhad Omid <farhadino@gmail.com>
2013-10-05 20:14:37 +0200

    wip viz module

---
+++ viz.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "viz.h"
+
+void viz_bar(float value, float max_val, int width) {
+	printf("TODO\n");
+}
---
+++ viz.h
@@ -0,0 +1,8 @@
+#ifndef viz_h
+#define viz_h
+
+#include "matrix.h"
+
+void viz_bar(float value, float max_val, int width);
+
+#endif
