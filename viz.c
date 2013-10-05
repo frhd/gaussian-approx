60432e8841fa88a38a940d2e1054509ccb4b8e5c
Farhad Omid <farhadino@gmail.com>
2013-10-05 21:13:09 +0200

    handle negative values in viz_bar

--- viz.c
+++ viz.c
@@ -5,11 +5,18 @@ void viz_bar(float value, float max_val, int width) {
 	int i, n, center;
 	if (max_val <= 0) max_val = 1.0;
 
-	center = width / 2;
-	n = (int)(value / max_val * (width / 2));
-	if (n > width / 2) n = width / 2;
-	if (n < 0) n = 0;
-	for (i = 0; i < center; i++) printf(" ");
-	for (i = 0; i < n; i++) printf("#");
-	printf("\n");
+	if (value >= 0) {
+		center = width / 2;
+		n = (int)(value / max_val * (width / 2));
+		if (n > width / 2) n = width / 2;
+		for (i = 0; i < center; i++) printf(" ");
+		for (i = 0; i < n; i++) printf("#");
+		printf("\n");
+	} else {
+		center = width / 2;
+		n = (int)(-value / max_val * (width / 2));
+		for (i = 0; i < center - n; i++) printf(" ");
+		for (i = 0; i < n; i++) printf("#");
+		printf("\n");
+	}
 }
