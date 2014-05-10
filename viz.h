9c0662da54fa0e4daff134a335305e247b4f9102
Farhad Omid <farhadino@gmail.com>
2014-05-10 19:23:17 +0200

    2d grid, start

--- viz.h
+++ viz.h
@@ -3,6 +3,14 @@
 
 #include "matrix.h"
 
+#define GRID_W 60
+#define GRID_H 30
+
+typedef struct {
+	char cells[GRID_H][GRID_W];
+	float xmin, xmax, ymin, ymax;
+} Grid;
+
 void viz_bar(float value, float max_val, int width);
 void viz_vector(Matrix v);
 void viz_gaussian_1d(float mean, float sigma, int width, int height);
