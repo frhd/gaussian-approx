a15cf54bf1461a116aace1722ee7eb49fc2518bc
Farhad Omid <farhadino@gmail.com>
2016-11-19 22:41:15 +0100

    implement csv header and row output

--- export.c
+++ export.c
@@ -12,14 +12,16 @@ FILE *export_open(const char *filename) {
 }
 
 void export_header_2d(FILE *f) {
-	/* TODO */
+	fprintf(f, "step,time,true_x,true_y,meas_x,meas_y,est_x,est_y,est_vx,est_vy,cov_xx,cov_yy,rmse\n");
 }
 
 void export_row_2d(FILE *f, int step, float t,
 	float true_x, float true_y, float meas_x, float meas_y,
 	float est_x, float est_y, float est_vx, float est_vy,
 	float cov_xx, float cov_yy, float rmse) {
-	/* TODO */
+	fprintf(f, "%d,%f,%f,%f,%f,%f,%f,%f,%f;%f,%f,%f,%f\n",
+		step, t, true_x, true_y, meas_x, meas_y,
+		est_x, est_y, est_vx, est_vy, cov_xx, cov_yy, rmse);
 }
 
 void export_close(FILE *f) {
