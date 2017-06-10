0a7ca906ac3a39f591507c100ca51ee484a5d655
Farhad Omid <farhadino@gmail.com>
2017-06-10 20:33:07 +0200

    fix csv delimiter, was mixing comma and semicolon

--- export.c
+++ export.c
@@ -19,7 +19,7 @@ void export_row_2d(FILE *f, int step, float t,
 	float true_x, float true_y, float meas_x, float meas_y,
 	float est_x, float est_y, float est_vx, float est_vy,
 	float cov_xx, float cov_yy, float rmse) {
-	fprintf(f, "%d,%f,%f,%f,%f,%f,%f,%f,%f;%f,%f,%f,%f\n",
+	fprintf(f, "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
 		step, t, true_x, true_y, meas_x, meas_y,
 		est_x, est_y, est_vx, est_vy, cov_xx, cov_yy, rmse);
 }
