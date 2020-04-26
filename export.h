e6963b6ff3839aa74a4d59499c1d107b8546c990
Farhad Omid <farhadino@gmail.com>
2020-04-26 14:17:41 +0200

    export header update for NIS

--- export.h
+++ export.h
@@ -8,7 +8,7 @@ void export_header_2d(FILE *f);
 void export_row_2d(FILE *f, int step, float t,
 	float true_x, float true_y, float meas_x, float meas_y,
 	float est_x, float est_y, float est_vx, float est_vy,
-	float cov_xx, float cov_yy, float rmse);
+	float cov_xx, float cov_yy, float rmse, float nis);
 void export_header_1d(FILE *f);
 void export_row_1d(FILE *f, int step, float t,
 	float true_x, float meas_x, float est_x, float est_v,
