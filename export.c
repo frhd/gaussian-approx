787f7bfa917a28a577c28b0c0ab197b89e479493
Farhad Omid <farhadino@gmail.com>
2020-05-23 18:05:42 +0200

    export nis to csv

--- export.c
+++ export.c
@@ -12,16 +12,16 @@ FILE *export_open(const char *filename) {
 }
 
 void export_header_2d(FILE *f) {
-	fprintf(f, "step,time,true_x,true_y,meas_x,meas_y,est_x,est_y,est_vx,est_vy,cov_xx,cov_yy,rmse\n");
+	fprintf(f, "step,time,true_x,true_y,meas_x,meas_y,est_x,est_y,est_vx,est_vy,cov_xx,cov_yy,rmse,nis\n");
 }
 
 void export_row_2d(FILE *f, int step, float t,
 	float true_x, float true_y, float meas_x, float meas_y,
 	float est_x, float est_y, float est_vx, float est_vy,
-	float cov_xx, float cov_yy, float rmse) {
-	fprintf(f, "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+	float cov_xx, float cov_yy, float rmse, float nis) {
+	fprintf(f, "%d,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
 		step, t, true_x, true_y, meas_x, meas_y,
-		est_x, est_y, est_vx, est_vy, cov_xx, cov_yy, rmse);
+		est_x, est_y, est_vx, est_vy, cov_xx, cov_yy, rmse, nis);
 }
 
 void export_header_1d(FILE *f) {
